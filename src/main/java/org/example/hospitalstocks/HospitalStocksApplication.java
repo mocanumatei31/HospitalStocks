59fa7bc7b77dd7127d25e7b7b58e402712ec662b
mat <128747016+mocanumatei31@users.noreply.github.com>
2024-05-30 11:48:59 +0300

    Initial commit

---
+++ src/main/java/org/example/hospitalstocks/HospitalStocksApplication.java
@@ -0,0 +1,13 @@
+package org.example.hospitalstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/hospitalstocks/HospitalStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hospitalstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
