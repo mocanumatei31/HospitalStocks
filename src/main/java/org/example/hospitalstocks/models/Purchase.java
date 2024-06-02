5850f98907e2f93d40518d18122790d05c53a1a9
mat <128747016+mocanumatei31@users.noreply.github.com>
2024-06-03 00:18:31 +0300

    Added table for purchases

---
+++ src/main/java/org/example/hospitalstocks/models/Purchase.java
@@ -0,0 +1,37 @@
+package org.example.hospitalstocks.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "PURCHASE")
+public class Purchase {
+    @Id
+    @Size(max = 50)
+    @Column(name = "ID", nullable = false, length = 50)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    @JoinColumn(name = "DRUG_ID")
+    private Drug drug;
+
+    @Column(name = "PRICE", precision = 9, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "QUANTITY")
+    private Integer quantity;
+
+    @Column(name = "DATE_BOUGHT")
+    private LocalDate dateBought;
+
+}
